921c9ca63ff13acbb8c58ca321093acba7da9503
Christopher Rooney <ctr_287@sbcglobal.net>
2021-06-08 18:01:15 -0400

    Update KinMS API, improve ln_lik printing
    KinMS Api changes:
    	Commented out parameters that are explicitly set to their default values.
    	This prevents problems when the default values change, as you would expect
    	same behavior for optional parameters not supplied to persist across versions.
    
    ln_lik printing:
    	the loglike function now only prints after 100 evaluations, configurable with
    	UVModelDisk.print_every = <integer>

--- UVmodeldisk.py
+++ UVmodeldisk.py
@@ -16,8 +16,11 @@ import numpy as np
 from KinMS import KinMS
 import uvutil
 from astropy.io import fits
-import pymultinest,emcee
+import pymultinest
+import emcee
 from galario.double import sampleImage
+from datetime import datetime
+
 
 class uvmodeldisk(object):
 	def __init__(self,xs,ys,vs,cellsize,dv,UVFITSfile,cube_template_file):
@@ -42,23 +45,23 @@ class uvmodeldisk(object):
 			It's best obtained by imaging the visibilities themselves, using mode='channel'. This ensures that the cell size is sufficient to suitable for the UV coverage.
 		Returns
 		-------
-			    
+				
 		"""
 		self.xs=xs   
-		self.ys=ys   #y-axis size of the model image (in arcsec)
+		self.ys=ys  # y-axis size of the model image (in arcsec)
 		self.vs=vs
 		self.cellsize=cellsize
 		self.dv=dv
-		self.UVFITSfile=UVFITSfile #'.uvfits'
-		self.cube_template_file=cube_template_file #'.fits'
-		self.sbrad=np.arange(0,2,.01)   #radius vector defining the light profile, sbprof is the light intensity evaluated at these radial values
-		self.velrad=self.sbrad          #use the same radius vector for the velocity definition, used by velprof to define the velocity profile
-		self.nsamps=1e5                 #number of clouds, should be sufficient
-		self.vis_complex_data, self.wgt_data = uvutil.visload(self.UVFITSfile) #load visibility and weights
+		self.UVFITSfile=UVFITSfile  # '.uvfits'
+		self.cube_template_file=cube_template_file  # '.fits'
+		self.sbrad=np.arange(0,2,.01)   # radius vector defining the light profile, sbprof is the light intensity evaluated at these radial values
+		self.velrad=self.sbrad          # use the same radius vector for the velocity definition, used by velprof to define the velocity profile
+		self.nsamps=1e5                 # number of clouds, should be sufficient
+		self.vis_complex_data, self.wgt_data = uvutil.visload(self.UVFITSfile)  # load visibility and weights
 		self.vis_complex_model_template=np.copy(self.vis_complex_data)
 		self.wgt_data=self.wgt_data.flatten()
 		self.good_vis=self.wgt_data>0
-		self.wgt_data=self.wgt_data[self.good_vis]  #get rid of flagged data
+		self.wgt_data=self.wgt_data[self.good_vis]  # get rid of flagged data
 		self.vis_complex_data=self.vis_complex_data.flatten()[self.good_vis]
 		self.modelheader = fits.getheader(self.cube_template_file)   
 		self.uu_cube, self.vv_cube, self.ww_cube = uvutil.uvload(self.UVFITSfile)
@@ -70,26 +73,29 @@ class uvmodeldisk(object):
 		self.Nypix_small=int(self.ys/self.cellsize)
 		self.Nchan_small=int(self.vs/self.dv)
 		#In this code we assume the continuum emission can be modelled as a 2D Gaussian, with known parameters. This is often the case and the continuum parameters are often precisely known. If they are not, include the continuum parameters as fitting parameters and make sure the visibilities cover a good range of continuum-only channels. 
-		self.y,self.x,self.zz=np.mgrid[:self.Nypix_small,:self.Nxpix_small,:self.Nchan_small]   #define a coordinate cube of the same size as the KinMS model, to make the continuum model, to be added in.
+		self.y,self.x,self.zz=np.mgrid[:self.Nypix_small,:self.Nxpix_small,:self.Nchan_small]   # define a coordinate cube of the same size as the KinMS model, to make the continuum model, to be added in.
 		self.offset_lnlike=0
 		self.xpos_center_padded,self.ypos_center_padded= int(self.Nxpix/2),int(self.Nypix/2)
+		self.recent_results = []
+		self.print_every = 100
+
 	def set_ellipt_gauss_continuum(self,cont_xcen,cont_ycen,cont_maj_FWHM,cont_min_FWHM,cont_posang,cont_flux):
 		"""
-		    This function defines the continuum model.
-		    Parameters
-		    ----------
-		    cont_xcen,cont_ycen: (float,float)
-		    	X-axis and Y-axis offset of continuum center from image center (in arcsec)
-		    cont_maj_FWHM,cont_min_FWHM: (float,float)
-		    	FWHM of the major and minor axes of the continuum 2D Gaussian (in arcsec)
-		    cont_posang: float
-		    	Position angle of the ellipse, counterclockwise from the positive x axis (in degrees)
-		    cont_flux: float
-		    	Integrated flux of the continuum emission (in mJy)
-		    Returns
-		    -------
-		    
-			    
+			This function defines the continuum model.
+			Parameters
+			----------
+			cont_xcen,cont_ycen: (float,float)
+				X-axis and Y-axis offset of continuum center from image center (in arcsec)
+			cont_maj_FWHM,cont_min_FWHM: (float,float)
+				FWHM of the major and minor axes of the continuum 2D Gaussian (in arcsec)
+			cont_posang: float
+				Position angle of the ellipse, counterclockwise from the positive x axis (in degrees)
+			cont_flux: float
+				Integrated flux of the continuum emission (in mJy)
+			Returns
+			-------
+			
+				
 		"""
 		xcen=self.x-cont_xcen/self.cellsize-self.Nxpix_small/2.
 		ycen=self.y-cont_ycen/self.cellsize-self.Nypix_small/2.
@@ -97,10 +103,13 @@ class uvmodeldisk(object):
 		ss=np.sin(cont_posang/180*np.pi)
 		r2=(xcen*cc+ycen*ss)**2/(cont_maj_FWHM/self.cellsize/2.355)**2+(xcen*ss-ycen*cc)**2/(cont_min_FWHM/self.cellsize/2.355)**2
 		self.model_cont=cont_flux*1e-3*np.exp(-r2/2)/(2*np.pi*(cont_maj_FWHM/self.cellsize/2.355)*(cont_min_FWHM/self.cellsize/2.355))
+	
 	def loglike_multinest(self,cube,ndim,npar):
 		return self.loglike(cube)
+	
 	def my_prior(self,cube):
 		return 0
+	
 	def loglike(self,cube):
 
 		'''
@@ -118,7 +127,7 @@ class uvmodeldisk(object):
 		vel_cen: float
 			The velocity center in km/s away from central channel, increasing vel_cen moves it to later channels, i.e. same direction as the channel ordering
 		inc: float
-		 	Inclination, is 0 for face on and 90 for edge-on
+			Inclination, is 0 for face on and 90 for edge-on
 		posang: float
 			Position angle starting with red emission from horizontal to the right (toward decreasing RA), and increasing counterclockwise (when we have a - in front of vmax). posang near 0, and - in front of vmax, means the emission moves right to left as channels increase. positive posang rotates this pattern toward the north, in a counterclockwise manner
 		x_cen, y_cen: (float,float)
@@ -147,39 +156,40 @@ class uvmodeldisk(object):
 		sbprof=np.exp(-self.sbrad**2/2/(bright_std/2.355)**2)
 		velprof=vmax*np.arctan(self.velrad/vel_scale)/np.pi*2
 		self.modelimage=KinMS(self.xs,
-				self.ys,
-				self.vs,
-				cellSize=self.cellsize,
-				dv=self.dv,
-				beamSize=0,
-				inc=inc,
-				gasSigma=gassigma,
-				sbProf=sbprof,
-				sbRad=self.sbrad,
-				velRad=self.velrad,
-				velProf=velprof,
-				diskThick=0,
-				cleanOut=True,
-				ra=0,
-				dec=0,
-				nSamps=self.nsamps,
-				posAng=posang,
-				intFlux=intflux,
-				inClouds=[],
-				vLOS_clouds=[],
-				flux_clouds=0,
-				vSys=0,
-				restFreq=115.271e9,
-				phaseCen=np.array([x_cen,y_cen]),
-				vOffset=vel_cen,
-				fixSeed=False,
-				vRadial=0,
-				vPosAng=0,
-				#vPhaseCen=np.array([x_cen,y_cen]), 
-				#That ^ was a workaround for a bug in KinMS that has now been fixed 
-				returnClouds=False,
-				gasGrav=False,
-				fileName=False)
+							  self.ys,
+							  self.vs,
+							  cellSize=self.cellsize,
+							  dv=self.dv,
+							  beamSize=0,
+							  inc=inc,
+							  gasSigma=gassigma,
+							  sbProf=sbprof,
+							  sbRad=self.sbrad,
+							  velRad=self.velrad,
+							  velProf=velprof,
+							  #diskThick=0,
+							  cleanOut=True,
+							  #ra=0,
+							  #dec=0,
+							  nSamps=self.nsamps,
+							  posAng=posang,
+							  intFlux=intflux,
+							  #inClouds=[],
+							  #vLOS_clouds=[],
+							  #flux_clouds=0,
+							  #vSys=0,
+							  #restFreq=115.271e9,
+							  phaseCent=np.array([x_cen,y_cen]),
+							  vOffset=vel_cen,
+							  #fixSeed=False,
+							  #inflowVel=0,
+							  #vPosAng=0,
+							  #vPhaseCen=np.array([x_cen,y_cen]), 
+							  #That ^ was a workaround for a bug in KinMS that has now been fixed 
+							  returnClouds=False,
+							  #massDist=[],
+							  #fileName=False
+							  )
 		model = model_cont+self.modelimage
 		xpos,ypos=self.xpos_center_padded,self.ypos_center_padded
 		model_padded=np.transpose(np.pad(model,((ypos-self.Nypix_small/2,self.Nypix-ypos-self.Nypix_small/2),(xpos-self.Nxpix_small/2,self.Nxpix-xpos-self.Nxpix_small/2),(0,0)),mode='constant'),(2,0,1))
@@ -194,22 +204,34 @@ class uvmodeldisk(object):
 			uushape = uu.shape
 			uu = uu.flatten()
 			vv = vv.flatten()
-			uu=uu.copy(order='C')  #This
-			vv=vv.copy(order='C') #this
-			modelimage=np.roll(np.flip(modelimage,axis=0),1,axis=0).copy(order='C')#.byteswap().newbyteorder()    #This
-			model_complex = sampleImage(modelimage, np.absolute(self.modelheader['CDELT1'])/180*np.pi, uu, vv)  #this uses galario
+			uu=uu.copy(order='C') 
+			vv=vv.copy(order='C') 
+			modelimage=np.roll(np.flip(modelimage,axis=0),1,axis=0).copy(order='C')  # .byteswap().newbyteorder()  
+			model_complex = sampleImage(modelimage, np.absolute(self.modelheader['CDELT1'])/180*np.pi, uu, vv)  # this uses galario
 			#model_complex = sample_vis.uvmodel(modelimage, modelheader, uu, vv, pcd)
 			vis_complex = model_complex.reshape(uushape)
 			vis_complex_model[:,:,chan,:]=vis_complex[:,:,0,:]
 		#replace_visibilities('HZ10_spw01_comb.uvfits','my_img_mod.fits','model_visib.uvfits')
 		#vis_complex_model,bb  = uvutil.visload('model_visib.uvfits')
 		vis_complex_model=vis_complex_model.flatten()[self.good_vis]
+		
 		def find_param(scale):
 			diff_all=np.abs(self.vis_complex_data-vis_complex_model*scale)
 			return np.sum(self.wgt_data*diff_all*diff_all)
-		ln_like=-0.5*find_param(1)
-		print(ln_like)
-		return ln_like+self.offset_lnlike
+		
+		ln_like=-0.5*find_param(1) + self.offset_lnlike
+		self.recent_results.append(ln_like)
+
+		if len(self.recent_results) > self.print_every:
+			rr = self.recent_results
+			print("average of last {} results: {:.2f} Max: {:.2f} median: {:.2f} at {}"
+				  .format(self.print_every, 
+						  np.mean(rr), 
+						  np.max(rr), 
+						  np.median(rr), 
+						  str(datetime.now())))
+		return ln_like
+
 	def run_Multinest(self,Nthreads,output_filename='temporary_model'):
 		'''
 		This function runs Multinest using the self.my_prior priors, which need to be appropriately set to a Multinest-type of priors.
@@ -224,7 +246,15 @@ class uvmodeldisk(object):
 		n_params = 10
 		from galario import double
 		double.threads(Nthreads)
-		pymultinest.run(self.loglike_multinest, self.my_prior, n_params, outputfiles_basename=output_filename, resume = True, verbose = True,sampling_efficiency='model')
+		pymultinest.run(self.loglike_multinest, 
+						self.my_prior, 
+						n_params, 
+						outputfiles_basename=output_filename, 
+						resume=True, 
+						verbose=True,
+						sampling_efficiency='model'
+						)
+	
 	def lnprob(self,cube):
 		'''
 		This function combines the emcee-type prior and the log likelihood to compute the log probability. Should be used with emcee.
@@ -233,6 +263,7 @@ class uvmodeldisk(object):
 			return self.my_prior(cube)+self.loglike(cube)
 		else:
 			return -np.inf
+	
 	def find_max_prob(self,starting_guess,Nthreads_galario):
 		'''
 		This function tries to find the maximum probability parameter values. This may be useful for ML fitting or, e.g., to start an MCMC sampler such as emcee.
@@ -255,6 +286,7 @@ class uvmodeldisk(object):
 		result=minimize(nll,starting_guess,method='Nelder-Mead')
 		print(result)
 		return result['x']
+	
 	def run_emcee(self,Nthreads_galario,Nthreads_emcee,nwalkers,starting_point,Nsteps,output_filename='temporary_model'):
 		'''
 		This function runs emcee to produce MCMC samples from the probability distribution defined by my_prior and loglike.
@@ -285,19 +317,18 @@ class uvmodeldisk(object):
 			print(idx,' of ',int(Nsteps/100))
 
 
-
 #Necessary in order to use the lnprob instance method within the emcee sampler, to make it picklable
 class picklable_boundmethod(object):
+	#Commented out setstate because it fails with syntax error.
+	#emcee is not supported by this version.
 	def __init__(self, mt):
 		self.mt = mt
 	def __getstate__(self):
 		return self.mt.im_self, self.mt.im_func.__name__
-	def __setstate__(self, (s,fn)):
-		self.mt = getattr(s, fn)
+	# def __setstate__(self, (s,fn)):
+	#     self.mt = getattr(s, fn)
 	def __call__(self, *a, **kw):
 		return self.mt(*a, **kw)
-
-
 
 '''
 #Example of priors for Multinest
